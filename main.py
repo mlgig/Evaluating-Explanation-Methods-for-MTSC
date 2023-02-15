1d6d72aca25b64ca989e6349ac5ac21502152784
davide <davide.serramazza@gmail.com>
2023-02-15 17:37:55 +0000

    notes for resNet with synths

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from sktime.transformations.panel.rocket import Rocket
 from sklearn.linear_model import RidgeClassifierCV, LogisticRegressionCV
 import timeit
 from utilities import *
+from sklearn.pipeline import make_pipeline
+from joblib import dump, load
 #from resnet_torch import ResNetBaseline
+
+
+
+import warnings
+warnings.filterwarnings("ignore")
 
 #TODO is there a better way to import the files?
 import sys
@@ -15,7 +22,7 @@ from DCAM import *
 from CNN_models import *
 
 def main():
-    all_data = load_data("MP")
+    all_data = load_data("synth")
 
     for dataset_name in all_data.keys():
         print("dataset ",dataset_name)
@@ -24,17 +31,24 @@ def main():
         # temp main for getting accuracy using synth datasets
         n_run = 5
 
+
         acc_res = []
         starttime = timeit.default_timer()
+        mid_channels = 128
         for i in range(n_run):
             train_dataloader,test_dataloader, n_channels, n_classes, device = transform_data4ResNet(data,dataset_name)
             # TODO check carefully data types in the hole process as label types?
             # TODO also check the conversion as .float() etc. Can I avoid some of them?
-            modelarch = dResNetBaseline(n_channels,mid_channels=64,num_pred_classes=n_classes).to(device)
-            model = ModelCNN(model=modelarch ,n_epochs_stop=30,device=device)# ,save_path='saved_model/resNet/'+dataset_name+str(i))
+            # TODO with synth data better to have 128 filters so i should change batch size to 32 both for train and test
+            modelarch = dResNetBaseline(n_channels,mid_channels=mid_channels,num_pred_classes=n_classes).to(device)
+            model = ModelCNN(model=modelarch ,n_epochs_stop=30,device=device,save_path='saved_model/resNet/'
+                    +dataset_name+"_nFilters_"+str(mid_channels)+"_"+str(i))
             acc = model.train(num_epochs=1000,train_loader=train_dataloader,test_loader=test_dataloader)
+            print(i,acc)
             acc_res.append(acc)
         print("\t resnet accuracy is ",np.sum(acc_res)/n_run, acc_res," time was ", (timeit.default_timer() - starttime)/n_run)
+
+        
 
         #TODO move above train and test pointers
         train_set = data["X_train"]
@@ -47,29 +61,30 @@ def main():
 
         for normal in [True,False]:
             acc_mini=[]
-            for _ in range(n_run):
+            for i in range(n_run):
 
                 # rocket
-                rocket = Rocket(normalise=normal)
-
-                parameters = rocket.fit(train_set)
-                X_train_trans = rocket.transform(train_set, parameters)
-                X_test_trans = rocket.transform(test_set,parameters)
-
-                cls = LogisticRegressionCV(solver='newton-cg',multi_class = 'multinomial', class_weight='balanced')
-                cls.fit(X_train_trans,data["y_train"])
-                acc = cls.score(X_test_trans,data["y_test"])
+                cls = make_pipeline(Rocket(normalise=normal),
+                                    #LogisticRegressionCV(solver='newton-cg',multi_class = 'multinomial', class_weight='balanced'))
+                                    RidgeClassifierCV(alphas=np.logspace(-3, 3, 10), normalize=True))
+                cls.fit(train_set,data["y_train"])
+                acc = cls.score(test_set,data["y_test"])
+                print(normal,i,acc)
                 acc_mini.append(acc)
+                #dump(cls,"saved_model/rocket/"+dataset_name+"_norm_"+str(normal)+"_"+str(i))
             print("\t rocket normal ",normal," accuracy is ",np.sum(acc_mini)/n_run," time was ", (timeit.default_timer() - starttime)/n_run)
 
 
         starttime = timeit.default_timer()
         for seql in ["fs","clf"]:
             acc_mrSEQL=[]
-            for _ in range(n_run):
+            for i in range(n_run):
                 model = MrSEQLClassifier(seql_mode=seql,symrep=['sax'])
                 model.fit(train_set,data["y_train"])
-                acc_mrSEQL.append( model.score(test_set,data["y_test"]) )
+                acc = model.score(test_set,data["y_test"])
+                print(acc)
+                acc_mrSEQL.append(acc)
+                dump(model,"saved_model/mrSEQL/"+dataset_name+"_seql_"+str(seql)+"_"+str(i))
             print("\t mrSeql ",seql," sax accuracy is ", np.sum(acc_mrSEQL)/n_run," time was ",  (timeit.default_timer() - starttime)/n_run)
 
 
